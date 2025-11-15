78d9c61847e1430fa51a4b1030bd9084ec23d376
api-asim <Asimbadawi18@gmail.com>
2025-11-15 09:51:22 +0200

    change Url value

--- src/utils/Url.tsx
+++ src/utils/Url.tsx
@@ -1,1 +1,6 @@
-export const Url = 'http://localhost:5000/api';
+
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname.startsWith('192.168.') || window.location.hostname.startsWith('10.');
+
+export const Url: string = isLocalhost
+    ? 'http://localhost:5000/api'               
+    : 'https://employee-api-wheat.vercel.app/api'; 
