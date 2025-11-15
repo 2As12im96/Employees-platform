7784e3c0cce9f5ca9348c5416742e6ec29187803
api-asim <Asimbadawi18@gmail.com>
2025-11-15 09:59:44 +0200

    update Url value

--- src/utils/Url.tsx
+++ src/utils/Url.tsx
@@ -1,8 +1,8 @@
 
-// const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname.startsWith('192.168.') || window.location.hostname.startsWith('10.');
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname.startsWith('192.168.') || window.location.hostname.startsWith('10.');
 
-// export const Url: string = isLocalhost
-//     ? 'http://localhost:5000/api'               
-//     : 'https://employee-api-wheat.vercel.app/api'; 
+export const Url: string = isLocalhost
+    ? 'http://localhost:5000/api'               
+    : 'https://employee-api-wheat.vercel.app/api'; 
 
-export const Url:string = 'https://employee-api-wheat.vercel.app/api';
+// export const Url:string = 'https://employee-api-wheat.vercel.app/api';
